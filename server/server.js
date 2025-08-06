6f1853b28cff59e360098836aad2e8822a15516a
rishabh jain <rishabh24272004@gmail.com>
2025-08-07 00:41:56 +0530

    cors added

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://yourdomain.com'] 
+    ? ['https://algoversefrontend.onrender.com/'] 
     : ['http://localhost:3000', 'http://localhost:3001'],
   credentials: true
 }));
