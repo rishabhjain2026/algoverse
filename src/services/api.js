34c831c8cdfd2a596ed874f8128107c55cc898b0
rishabh jain <rishabh24272004@gmail.com>
2025-08-07 00:17:23 +0530

    api url added in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@
 const API_BASE_URL =
   window.location.hostname === 'localhost'
     ? 'http://localhost:5000/api'
-    : 'https://eco-trackers1.onrender.com/';
+    : 'https://eco-trackers1.onrender.com/api';
 
 class ApiService {
   constructor() {
